268e20d307eda74902265ebafaf11021e6ce4f48
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-06 14:43:11 +0330

    Level 14

---
+++ natas14/hack.py
@@ -0,0 +1,14 @@
+import requests
+
+username = "natas14"
+password = "Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1"
+
+url = "http://natas14.natas.labs.overthewire.org/"
+
+session = requests.Session()
+
+response = sessions.post(url, data = {"username" : "salam", "password": "banoo"}) 
+
+print(response.body)
+
+
