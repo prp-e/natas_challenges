72ed165150067d0c051313d19977091c00d94ee6
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-06 14:56:34 +0330

    Natas 15

---
+++ natas15/hack.py
@@ -0,0 +1,11 @@
+import requests
+
+username = "natas15"
+password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
+
+url = "http://natas15.natas.lab.overthewire.org/"
+
+session = requests.Session()
+response = session.get(url, auth=(username, password))
+
+print(response.text)
