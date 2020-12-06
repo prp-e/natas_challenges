3622e9336ae46fc1c097d59a834492dda09ae7ff
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-06 14:59:10 +0330

    Errors in URL fixed

--- natas15/hack.py
+++ natas15/hack.py
@@ -3,7 +3,7 @@ import requests
 username = "natas15"
 password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
 
-url = "http://natas15.natas.lab.overthewire.org/"
+url = "http://natas15.natas.labs.overthewire.org/"
 
 session = requests.Session()
 response = session.post(url, auth=(username, password))
